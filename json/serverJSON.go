ae3f235785cf72c5c66bffb135225e1c9eeed6c8
zhukai <1842159520@qq.com>
2017-09-27 12:30:56 +0800

    添加解析json 生成json方法

--- json/serverJSON.go
+++ json/serverJSON.go
@@ -3,7 +3,6 @@ package main
 import (
 	"fmt"
 	"encoding/json"
-
 )
 
 type Server struct {
@@ -13,44 +12,72 @@ type Server struct {
 type Serverslice struct {
 	Servers []Server
 }
-func main() {
-	//var s Serverslice
-	//str := `{"servers":
-	//		[{"serverName":"Shanghai_VPN","serverIP":"127.0.0.1"},
-	//		{"serverName":"Beijing_VPN","serverIP":"127.0.0.2"}]}`
-	//json.Unmarshal([]byte(str),&s)
-	//fmt.Printf("%+v", s)
-	var f interface{}
-	b := []byte(`{"Name":"Wednesday","Age":6,"Parents":["Gomez","Morticia"]}`)
-	err := json.Unmarshal(b,&f)
-	if err!= nil {
-		fmt.Println(err)
+//func main() {
+//	//var s Serverslice
+//	//str := `{"servers":
+//	//		[{"serverName":"Shanghai_VPN","serverIP":"127.0.0.1"},
+//	//		{"serverName":"Beijing_VPN","serverIP":"127.0.0.2"}]}`
+//	//json.Unmarshal([]byte(str),&s)
+//	//fmt.Printf("%+v", s)
+//	var f interface{}
+//	b := []byte(`{"Name":"Wednesday","Age":6,"Parents":["Gomez","Morticia"]}`)
+//	err := json.Unmarshal(b,&f)
+//	if err!= nil {
+//		fmt.Println(err)
+//	}
+//	m := f.(map[string]interface{})
+//	//js ,_ := simplejson.NewJson(b)
+//	//
+//	//fmt.Println(js.Get("Name").String())
+//
+//
+//	//fmt.Println(arr)
+//
+//	for k,v := range m  {
+//		switch vv:= v.(type){
+//		case string:
+//			fmt.Println(k,"is string",vv)
+//		case int:
+//			fmt.Println(k,"is int",vv)
+//		case []interface{}:
+//			fmt.Println(k,"is []interface{}",vv)
+//			for kk, vvv := range vv {
+//				fmt.Println(kk, vvv)
+//			}
+//		default:
+//			fmt.Println(k,vv)
+//			//fmt.Println(k, "is of a type I don't know how to handle")
+//		}
+//
+//	}
+//
+//}
+//
+func main(){
+	var s Serverslice
+	s.Servers = append(s.Servers, Server{ServerName: "Shanghai_VPN", ServerIP: "127.0.0.1"})
+	s.Servers = append(s.Servers, Server{ServerName: "Shanghai_VPN", ServerIP: "127.0.0.2"})
+
+	ret := MarshalJson(s)
+	UnmarshalJson([]byte(ret),&s)
+	fmt.Println(ret)
+	for _, v := range s.Servers {
+		fmt.Printf("%+v",v)
 	}
-	m := f.(map[string]interface{})
-	//js ,_ := simplejson.NewJson(b)
-	//
-	//fmt.Println(js.Get("Name").String())
-
-
-	//fmt.Println(arr)
-
-	for k,v := range m  {
-		switch vv:= v.(type){
-		case string:
-			fmt.Println(k,"is string",vv)
-		case int:
-			fmt.Println(k,"is int",vv)
-		case []interface{}:
-			fmt.Println(k,"is []interface{}",vv)
-			for kk, vvv := range vv {
-				fmt.Println(kk, vvv)
-			}
-		default:
-			fmt.Println(k,vv)
-			//fmt.Println(k, "is of a type I don't know how to handle")
-		}
-
+}
+func UnmarshalJson(b []byte, beans interface{}){
+	err := json.Unmarshal(b,beans)
+	if err != nil {
+		fmt.Printf("--->err : %v",err)
+		return
 	}
-
+}
+func MarshalJson(beans interface{}) string{
+	b, err := json.Marshal(beans)
+	if err != nil {
+		fmt.Printf("--->err : %v",err)
+		return ""
+	}
+	return string(b)
 }
 
