287320ed1452db06d47c71d72933cb4ca9c3352a
zhukai <1842159520@qq.com>
2017-09-27 10:43:52 +0800

    断言处理未知结构体

--- json/serverJSON.go
+++ json/serverJSON.go
@@ -3,7 +3,7 @@ package main
 import (
 	"fmt"
 	"encoding/json"
-	
+
 )
 
 type Server struct {
@@ -14,20 +14,19 @@ type Serverslice struct {
 	Servers []Server
 }
 func main() {
-	var s Serverslice
-	str := `{"servers":
-			[{"serverName":"Shanghai_VPN","serverIP":"127.0.0.1"},
-			{"serverName":"Beijing_VPN","serverIP":"127.0.0.2"}]}`
-	json.Unmarshal([]byte(str),&s)
-	fmt.Printf("%+v", s)
-	//var f interface{}
-	////b := []byte(`{"name":"zhukai","age":18,"sex":"男"}`)
-	//b := []byte(`{"Name":"Wednesday","Age":6,"Parents":["Gomez","Morticia"]}`)
-	//err := json.Unmarshal(b,&f)
-	//if err!= nil {
-	//	fmt.Println(err)
-	//}
-	////m := f.(map[string]interface{})
+	//var s Serverslice
+	//str := `{"servers":
+	//		[{"serverName":"Shanghai_VPN","serverIP":"127.0.0.1"},
+	//		{"serverName":"Beijing_VPN","serverIP":"127.0.0.2"}]}`
+	//json.Unmarshal([]byte(str),&s)
+	//fmt.Printf("%+v", s)
+	var f interface{}
+	b := []byte(`{"Name":"Wednesday","Age":6,"Parents":["Gomez","Morticia"]}`)
+	err := json.Unmarshal(b,&f)
+	if err!= nil {
+		fmt.Println(err)
+	}
+	m := f.(map[string]interface{})
 	//js ,_ := simplejson.NewJson(b)
 	//
 	//fmt.Println(js.Get("Name").String())
@@ -35,23 +34,23 @@ func main() {
 
 	//fmt.Println(arr)
 
-	//for k,v := range m  {
-	//	switch vv:= v.(type){
-	//	case string:
-	//		fmt.Println(k,"is string",vv)
-	//	case int:
-	//		fmt.Println(k,"is int",vv)
-	//	case []interface{}:
-	//		fmt.Println(k,"is []interface{}",vv)
-	//		for kk, vvv := range vv {
-	//			fmt.Println(kk, vvv)
-	//		}
-	//	default:
-	//		fmt.Println(k,vv)
-	//		//fmt.Println(k, "is of a type I don't know how to handle")
-	//	}
-	//
-	//}
+	for k,v := range m  {
+		switch vv:= v.(type){
+		case string:
+			fmt.Println(k,"is string",vv)
+		case int:
+			fmt.Println(k,"is int",vv)
+		case []interface{}:
+			fmt.Println(k,"is []interface{}",vv)
+			for kk, vvv := range vv {
+				fmt.Println(kk, vvv)
+			}
+		default:
+			fmt.Println(k,vv)
+			//fmt.Println(k, "is of a type I don't know how to handle")
+		}
+
+	}
 
 }
 
